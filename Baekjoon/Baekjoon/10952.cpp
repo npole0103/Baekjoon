22cd6c664d32ad5870f9713fa322366cfec97647
npole0103 <npole0103@naver.com>
2020-12-30 16:14:16 +0900

    class 1 silver

---
+++ Baekjoon/Baekjoon/10952.cpp
@@ -0,0 +1,11 @@
+//#include <iostream> // A + B - 5
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b;
+//	while (cin >> a >> b && a != 0 && b != 0)
+//		cout << a + b << endl;
+//
+//	return 0;
+//}
---
+++ Baekjoon/Baekjoon/11720.cpp
@@ -0,0 +1,18 @@
+//#include <iostream> //숫자의 합
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int num;
+//	string str;
+//	int sum = 0;
+//
+//	cin >> num >> str;
+//
+//	for (int i = 0; i < num; i++)
+//		sum += str[i] - 48;
+//	
+//	cout << sum << endl;
+//}
