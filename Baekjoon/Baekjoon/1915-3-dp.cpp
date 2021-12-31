a89b47c4dc9a434199d45651b9a7147ac1604beb
npole0103 <npole0103@naver.com>
2021-12-31 12:21:50 +0900

    21/12/31 5 Questions

--- Baekjoon/Baekjoon/1915-3-dp.cpp
+++ Baekjoon/Baekjoon/1915-3-dp.cpp
@@ -1,50 +1,48 @@
-/*
-#include <iostream> //가장 큰 정사각형
-#include <string>
-
-#define MIN(X, Y) (X > Y ? Y : X)
-
-using namespace std;
-
-int main()
-{
-	int row;
-	int col;
-	int ans = 0;
-	
-	cin >> row;
-	cin >> col;
-	
-	string str[row];
-	int arr[row][col];
-	int dparr[row][col];
-	
-	//input
-	for(int i = 0 ; i < row ; i++)
-		cin >> str[i];
-	
-	for(int i = 0 ; i < row ; i++)
-		for(int j = 0 ; j < col ; j++)
-		{
-			arr[i][j] = (int)str[i][j] - 48;
-			dparr[i][j] = (int)str[i][j] - 48;
-		}
-
-	//let's using the dparr to find max_square_value
-	//현재 위치에서 (-1, 0), (0, -1), (-1, -1) 값들 비교해서 그 중 제일 작은 dp값을 (0 ,0) dp에 넣음.
-	for(int i = 1; i < row ; i++)
-		for(int j = 1; j < col; j++)
-			if(arr[i][j] == 1)
-				dparr[i][j] = MIN(MIN(dparr[i-1][j], dparr[i-1][j-1]), MIN(dparr[i][j-1], dparr[i-1][j-1])) + 1;
-	
-	//find the max in dparr
-	for(int i = 0; i < row ; i++)
-		for(int j = 0; j < col; j++)
-			if(dparr[i][j] > ans)
-				ans = dparr[i][j];
-
-	cout << ans*ans << endl;
-
-	return 0;
-}
-*/
+//#include <iostream> //가장 큰 정사각형
+//#include <string>
+//
+//#define MIN(X, Y) (X > Y ? Y : X)
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int row;
+//	int col;
+//	int ans = 0;
+//	
+//	cin >> row;
+//	cin >> col;
+//	
+//	string str[row];
+//	int arr[row][col];
+//	int dparr[row][col];
+//	
+//	//input
+//	for(int i = 0 ; i < row ; i++)
+//		cin >> str[i];
+//	
+//	for(int i = 0 ; i < row ; i++)
+//		for(int j = 0 ; j < col ; j++)
+//		{
+//			arr[i][j] = (int)str[i][j] - 48;
+//			dparr[i][j] = (int)str[i][j] - 48;
+//		}
+//
+//	//let's using the dparr to find max_square_value
+//	//현재 위치에서 (-1, 0), (0, -1), (-1, -1) 값들 비교해서 그 중 제일 작은 dp값을 (0 ,0) dp에 넣음.
+//	for(int i = 1; i < row ; i++)
+//		for(int j = 1; j < col; j++)
+//			if(arr[i][j] == 1)
+//				dparr[i][j] = MIN(MIN(dparr[i-1][j], dparr[i-1][j-1]), MIN(dparr[i][j-1], dparr[i-1][j-1])) + 1;
+//	
+//	//find the max in dparr
+//	for(int i = 0; i < row ; i++)
+//		for(int j = 0; j < col; j++)
+//			if(dparr[i][j] > ans)
+//				ans = dparr[i][j];
+//
+//	cout << ans*ans << endl;
+//
+//	return 0;
+//}
--- Baekjoon/Baekjoon/1915.cpp
+++ Baekjoon/Baekjoon/1915.cpp
@@ -1,62 +1,60 @@
-/*
-#include <iostream> //1915 가장 큰 정사각형
-using namespace std;
-
-int main()
-{
-	int n , m;
-	cin >> n >> m;
-	int count = 0;
-	int result = 0;
-	
-	int rect[1001][1001] = {2, };
-	
-	for(int i = 1 ; i <= n ; i++) // 현재 문제, 입력 받는 것이 공백 구별 없이 받음
-		for(int j = 1 ; j <= m; j++)
-			cin >> rect[i][j];
-	
-	
-	for(int i = 1; i <= n ; i++)
-		for(int j = 1; j <= m; i++)
-		{
-			endloop:
-			{
-				if(rect[i][j] == 1) //1이 표시된 구간 발견시
-				{
-					for(int k = j ; rect[i][k] != 1 ; k++) //몇 짜리 사각형인지
-						count += 1;
-
-					check_loop:
-					{
-						for(int l = i; l <= count ; l++)
-							for(int o = j; o <= count ; o++)
-							{
-								if(count == 1) //사각형이 1x1짜리라면
-								{
-									result = 1;
-									goto endloop;
-								}
-
-								if(rect[count][count] == 1) // count * count 가 전부 1로 표시되어있다면
-									result = count;
-								else if( rect[l][o] == 1)
-									continue;
-								else if( rect[l][o] != 1) //1이 아니라면
-								{
-									count -= 1; // count - 1 정사각형 검사
-									goto check_loop;
-								}
-							}
-					}
-
-				}
-				else
-					continue;
-			}
-		}
-	
-	
-	
-	return 0;
-}
-*/
+//#include <iostream> //1915 가장 큰 정사각형
+//using namespace std;
+//
+//int main()
+//{
+//	int n , m;
+//	cin >> n >> m;
+//	int count = 0;
+//	int result = 0;
+//	
+//	int rect[1001][1001] = {2, };
+//	
+//	for(int i = 1 ; i <= n ; i++) // 현재 문제, 입력 받는 것이 공백 구별 없이 받음
+//		for(int j = 1 ; j <= m; j++)
+//			cin >> rect[i][j];
+//	
+//	
+//	for(int i = 1; i <= n ; i++)
+//		for(int j = 1; j <= m; i++)
+//		{
+//			endloop:
+//			{
+//				if(rect[i][j] == 1) //1이 표시된 구간 발견시
+//				{
+//					for(int k = j ; rect[i][k] != 1 ; k++) //몇 짜리 사각형인지
+//						count += 1;
+//
+//					check_loop:
+//					{
+//						for(int l = i; l <= count ; l++)
+//							for(int o = j; o <= count ; o++)
+//							{
+//								if(count == 1) //사각형이 1x1짜리라면
+//								{
+//									result = 1;
+//									goto endloop;
+//								}
+//
+//								if(rect[count][count] == 1) // count * count 가 전부 1로 표시되어있다면
+//									result = count;
+//								else if( rect[l][o] == 1)
+//									continue;
+//								else if( rect[l][o] != 1) //1이 아니라면
+//								{
+//									count -= 1; // count - 1 정사각형 검사
+//									goto check_loop;
+//								}
+//							}
+//					}
+//
+//				}
+//				else
+//					continue;
+//			}
+//		}
+//	
+//	
+//	
+//	return 0;
+//}
