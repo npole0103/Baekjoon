5a9b3b08a2604418a07f77133a5fadec42de3dde
npole0103 <npole0103@naver.com>
2022-01-04 22:39:06 +0900

    22/01/04 - 3 Questions

--- Baekjoon/Baekjoon/9935-6-stack.cpp
+++ Baekjoon/Baekjoon/9935-6-stack.cpp
@@ -14,10 +14,10 @@
 //	int bombSize = 0;
 //	char bomblast = 0;
 //
-//	cin >> str;
-//	cin >> bomb;
+//	cin >> str; //원본 입력받음
+//	cin >> bomb; //폭탄 문자열 입력받음.
 //	bombSize = bomb.size();
-//	bomblast = bomb[bombSize - 1];
+//	bomblast = bomb[bombSize - 1]; //해당 문자의 마지막 문자
 //
 //	for (int i = 0; i < str.size(); i++)
 //	{
@@ -25,9 +25,9 @@
 //
 //		if (bomblast == result.top() && i >= bombSize - 1) // 뒷글자가 bomb이랑 같다면
 //		{
-//			for (int j = bombSize - 1; j >= 0; j--) //뒷글자 검사 반복문
+//			for (int j = bombSize - 1; j >= 0; j--) // 뒷글자 검사 반복문
 //			{
-//				if (result.size() != 0 && result.top() == bomb[j]) //같다면
+//				if (result.size() != 0 && result.top() == bomb[j]) // 같다면
 //				{
 //					temp.push(result.top());
 //					result.pop();
@@ -35,13 +35,13 @@
 //				else
 //					break;
 //
-//				if (j == 0) // j == 0이 될 때까지 즉 bomb이랑 일치한다는
+//				if (j == 0) // j == 0이 될 때까지 즉 bomb이랑 일치한다는 뜻
 //				{
-//					while(temp.size() != 0)
+//					while(temp.size() != 0) //temp에 빼놨던 문자열 모두 제거(bomb이기 때문에 원본에 다시 넣지않을 것임)
 //						temp.pop();
 //				}
 //			}
-//			while (temp.size() != 0)
+//			while (temp.size() != 0) //temp에 빼놨던 문자열을 다시 넣어주는 작업.
 //			{	
 //				result.push(temp.top());
 //				temp.pop();
@@ -59,7 +59,7 @@
 //			result.pop();
 //		}
 //		
-//		while (result2.size() != 0)
+//		while (result2.size() != 0) //출력
 //		{
 //			cout << result2.top();
 //			result2.pop();
